326c74885d4c70df9cddceb952165691cf6ee800
laiso <laiso@lai.so>
2020-05-12 00:33:21 +0700

    v1.0.2

---
+++ README.md
@@ -0,0 +1,17 @@
+# unnote
+
+![](https://i.imgur.com/hYgfrYH.png)
+
+note.comの記事データをダウンロードするツールです。
+
+```bash
+npx unnote export $USERNAME
+```
+
+ブラウザ向けエンドポイントを利用し、`data/` ディレクトリ以下に独自形式のJSONデータを書き出します。
+
+![](https://i.imgur.com/29jVcXu.png)
+
+使用の前にnote.comの[利用規約](https://note.com/terms)を確認してください。
+
+![](https://i.imgur.com/gkcaNhC.png)
---
+++ index.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+const got = require('got');
+const fs = require('fs');
+fs.mkdirSync('data/note', {recursive: true})
+
+async function unnote(username, page = 1, counter = 0) {
+  try {
+    const response = await got(`https://note.com/api/v2/creators/${username}/contents?kind=note&page=${page}`,
+        {responseType: 'json'}
+    );
+    fs.writeFile(`data/contents-${page}.json`, JSON.stringify(response.body.data), (err => {
+      if (err) console.error(err)
+    }))
+
+    const keys = response.body.data.contents.map(c => c.key)
+    for (const key of keys) {
+      const note = await getNote(key)
+      fs.writeFile(`data/note/${key}.json`, JSON.stringify(note), (err => {
+        if (err) console.error(err)
+      }))
+      console.info(`${note.publish_at} / ${note.tweet_text}`)
+      counter += 1
+      await sleep(1000)
+    }
+
+    if (response.body.data.isLastPage) {
+      console.info(`written out about ${counter} notes`)
+    } else {
+      await sleep(1000)
+      unnote(username, page + 1)
+    }
+  } catch (error) {
+    console.log(error.response.body);
+    process.exit(-1)
+  }
+}
+
+async function getNote(key) {
+  const response = await got(`https://note.com/api/v1/notes/${key}`,
+      {responseType: 'json'}
+  )
+  return response.body.data
+}
+
+// https://qiita.com/asa-taka/items/888bc5a1d7f30ee7eda2
+const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
+
+require('yargs')
+    .command('export [username] [page]', 'start the program', (yargs) => {
+      yargs
+          .positional('username', {
+            describe: 'user name'
+          })
+          .positional('page', {
+            describe: 'page to start',
+            default: 1
+          })
+    }, (argv) => {
+      if (!argv.username) {
+        console.error(`invalid opts: run it as 'export [username]'`)
+        process.exit(-1)
+      }
+      unnote(argv.username, argv.page);
+    })
+    .argv
